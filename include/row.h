ab355059ef0e6c86713c5acefe32f4a24765e357
Amber Taveras <taveras.amber@gmail.com>
2026-01-06 21:31:07 -0500

    adds better accessors to row struct and tests for row

--- include/row.h
+++ include/row.h
@@ -11,26 +11,93 @@
 
 using RowVariant = std::variant<int64_t, double, std::string>;
 
+template <typename T>
+concept RowType = requires(RowVariant v) { std::get<T>(v); };
+
 struct Row {
   std::unordered_map<std::string, RowVariant> data;
 
-  template <typename T>
-  T get(const std::string& column_name) const {
+  Row() = default;
+  explicit Row(const std::unordered_map<std::string, RowVariant>& d)
+      : data(d) {}
+  explicit Row(std::unordered_map<std::string, RowVariant>&& d)
+      : data(std::move(d)) {};
+
+  Row(std::initializer_list<std::pair<const std::string, RowVariant>> il)
+      : data(il.begin(), il.end()) {}
+
+  template <RowType T>
+  std::optional<T> get(const std::string& column_name) const {
     auto it{data.find(column_name)};
     if (it == data.end()) {
-      return Utils::get_null<T>();
+      return std::nullopt;
     }
 
-    return (*std::get_if<T>(&it->second));
+    const T* value{std::get_if<T>(&it->second)};
+    if (!value) {
+      throw std::bad_variant_access();
+    }
+
+    return *value;
   }
 
-  const RowVariant& operator[](const std::string& column_name) const {
-    return data.at(column_name);
+  template <RowType T>
+  T at(const std::string& column_name) const {
+    const T* value{std::get_if<T>(&data.at(column_name))};
+    if (!value) {
+      throw std::bad_variant_access();
+    }
+    return *value;
   }
 
-  RowVariant& operator[](const std::string& column_name) {
-    return data.at(column_name);
+  template <RowType T>
+  Row& set(const std::string& column_name, T value) {
+    auto it{data.find(column_name)};
+    if (it != data.end()) {
+      if (!std::holds_alternative<T>(
+              it->second)) {  // if existing, restrict to same data type
+        throw std::bad_variant_access();
+      }
+      it->second = std::move(value);
+    } else {
+      data[column_name] = std::move(value);
+    }
+
+    return *this;
   }
+
+  template <RowType T>
+  Row& update(const std::string& column_name, T value) {
+    auto it{data.find(column_name)};
+    if (it == data.end()) {
+      throw std::out_of_range("column: " + column_name + " not found");
+    } else {
+      if (!std::holds_alternative<T>(
+              it->second)) {  // restrict to same data type
+        throw std::bad_variant_access();
+      }
+      it->second = std::move(value);
+      return *this;
+    }
+  }
+
+  bool contains(const std::string& column_name) const {
+    return data.contains(column_name);
+  }
+  bool empty() { return data.empty(); }
+  size_t size() { return data.size(); }
+
+  using iterator =
+      typename std::unordered_map<std::string, RowVariant>::iterator;
+  using const_iterator =
+      typename std::unordered_map<std::string, RowVariant>::const_iterator;
+
+  iterator begin() { return data.begin(); }
+  const_iterator begin() const { return data.begin(); }
+  const_iterator cbegin() const noexcept { return data.cbegin(); }
+  iterator end() { return data.end(); }
+  const_iterator end() const { return data.end(); }
+  const_iterator cend() const noexcept { return data.cend(); }
 
   friend std::ostream& operator<<(std::ostream& os, const Row& row) {
     size_t count{};
---
+++ tests/test_row.cpp
@@ -0,0 +1,89 @@
+#include <gtest/gtest.h>
+
+#include "row.h"
+
+class RowTest : public ::testing::Test {
+ protected:
+  std::unordered_map<std::string, RowVariant> row_data{{"name", "test"},
+                                                       {"age", 100}};
+
+  Row row{row_data};
+  Row empty_row{};
+
+  Row row_via_initializer{{"name", "Amber"},
+                          {"age", 28},
+                          {"gpa", 3.98},
+                          {"profession", "Software Engineer"}};
+};
+
+// constructor with empty, single, multiple, null
+TEST_F(RowTest, ConstructorInitializesCorrectly) {
+  EXPECT_TRUE(empty_row.empty());
+  EXPECT_FALSE(row.empty());
+
+  for (const auto& [key, value] : row_data) {
+    EXPECT_TRUE(row.contains(key));
+
+    std::visit(
+        [&](const auto& expected) {
+          using T = std::decay_t<decltype(expected)>;
+          EXPECT_EQ(row.at<T>(key), expected);
+        },
+        value);
+  }
+}
+
+TEST_F(RowTest, InitializerListConstructsCorrectly) {
+  EXPECT_EQ(row_via_initializer.at<std::string>("name"), "Amber");
+  EXPECT_EQ(row_via_initializer.at<int64_t>("age"), 28);
+  EXPECT_EQ(row_via_initializer.at<double>("gpa"), 3.98);
+  EXPECT_EQ(row_via_initializer.at<std::string>("profession"),
+            "Software Engineer");
+}
+
+TEST_F(RowTest, GetReturnsOptional) {
+  auto value_opt{row.get<std::string>("name")};
+  EXPECT_TRUE(value_opt.has_value());
+
+  EXPECT_THROW(row.get<std::string>("age"), std::bad_variant_access);
+
+  auto not_found_opt{row.get<std::string>("profession")};
+  EXPECT_FALSE(not_found_opt.has_value());
+}
+
+TEST_F(RowTest, AtThrowsOrReturnsValue) {
+  auto name{row.at<std::string>("name")};
+  EXPECT_EQ(name, std::get<std::string>(row_data.at("name")));
+
+  EXPECT_THROW(row.at<std::string>("age"), std::bad_variant_access);
+
+  EXPECT_THROW(row.at<std::string>("profession"), std::out_of_range);
+}
+
+TEST_F(RowTest, SetAddsOrUpdatesAValue) {
+  std::string new_name{"testing"};
+  row.set<std::string>("name", new_name);
+  EXPECT_EQ(row.at<std::string>("name"), new_name);
+
+  double salary{75000.75};
+  row.set<double>("salary", salary);
+  EXPECT_TRUE(row.contains("salary"));
+  EXPECT_EQ(row.at<double>("salary"), salary);
+}
+
+TEST_F(RowTest, UpdateThrowsOrUpdates) {
+  std::string new_name{"testing"};
+  row.update<std::string>("name", new_name);
+  EXPECT_EQ(row.at<std::string>("name"), new_name);
+
+  EXPECT_THROW(row.update<double>("salary", 100.00), std::out_of_range);
+  EXPECT_FALSE(row.contains("salary"));
+}
+
+TEST_F(RowTest, SetAndUpdateDoNotModifyExistingType) {
+  int64_t invalid_type{5};
+
+  EXPECT_THROW(row.set<int64_t>("name", invalid_type), std::bad_variant_access);
+  EXPECT_THROW(row.update<int64_t>("name", invalid_type),
+               std::bad_variant_access);
+}
